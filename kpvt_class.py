8fb3d6efcec65bd010a8ea6fe7437dc161d0fa0d
Zach Werginz <zachary.werginz@snc.edu>
2016-07-06 17:19:53 -0500

    Added Calibration Paper and updated class to include SPMG

--- kpvt_class.py
+++ kpvt_class.py
@@ -16,7 +16,7 @@ from collections import namedtuple
 
 Pair = namedtuple('Pair', 'x y')
 
-__all__ = ["Ch512Map"]
+__all__ = ["Ch512Map", "SPMG"]
 
 class Ch512Map(sunpy.map.GenericMap):
     """KPVT 512 Channel Image Map.
@@ -29,7 +29,6 @@ class Ch512Map(sunpy.map.GenericMap):
 
         # Any Ch512 Instrument specific keyword manipulation
         self.meta['detector'] = "512"
-        self._fix_dsun()
         self._nickname = str(self.detector) + "" + str(self.measurement)
         if self.meta['cunit1'] == 'ARC-SEC':
             self.meta['cunit1'] = 'arcsec'
@@ -50,23 +49,56 @@ class Ch512Map(sunpy.map.GenericMap):
                     self.meta['cdelt2'] * self.spatial_units.y / u.pixel * self.meta['CRR_SCLY'])
 
     @property
-    def dsun(self):
-        """ Solar radius in arc-seconds at 1 au
-            previous value radius_1au = 959.644
-            radius = constants.average_angular_size
-            There are differences in the keywords in the test FITS data and in
-            the Helioviewer JPEG2000 files.  In both files, MDI stores the
-            the radius of the Sun in image pixels, and a pixel scale size.
-            The names of these keywords are different in the FITS versus the
-            JP2 file.  The code below first looks for the keywords relevant to
-            a FITS file, and then a JPEG2000 file.  For more information on
-            MDI FITS header keywords please go to http://soi.stanford.edu/,
-            http://soi.stanford.edu/data/ and
-            http://soi.stanford.edu/magnetic/Lev1.8/ .
-        """
+    def rsun_obs(self):
+        """ KPVT Magnetograms use a different keyword for distance to sun"""
         return self.meta['EPH_R0'] * u.arcsec
+
+    @property
+    def dsun(self):
+        """ KPVT at earth"""
+        dsun = sun.sunearth_distance(self.date).to(u.m)
+
+        return u.Quantity(dsun, 'm') 
 
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
         """Determines if header corresponds to an 512 Channel image"""
         return header.get('instrume') == '512-CH-MAG'
+
+class SPMGMap(sunpy.map.GenericMap):
+    """KPVT SPMG Channel Image Map.
+
+    """
+
+    def __init__(self, data, header, **kwargs):
+
+        super(SPMGMap, self).__init__(data, header, **kwargs)
+
+        # Any Ch512 Instrument specific keyword manipulation
+        if self.meta['cunit1'] == 'ARC-SEC':
+            self.meta['cunit1'] = 'arcsec'
+        if self.meta['cunit2'] == 'ARC-SEC':
+            self.meta['cunit2'] = 'arcsec'
+
+    @property
+    def rsun_obs(self):
+        """ KPVT Magnetograms use a different keyword for distance to sun"""
+        return self.meta['EPH_R0'] * u.arcsec
+
+    @property
+    def dsun(self):
+        """ KPVT at earth"""
+        dsun = sun.sunearth_distance(self.date).to(u.m)
+
+        return u.Quantity(dsun, 'm') 
+
+    @property
+    def scale(self):
+
+        return Pair(self.meta['cdelt1'] * self.spatial_units.x / u.pixel * self.meta['CRR_SCLX'],
+                    self.meta['cdelt2'] * self.spatial_units.y / u.pixel * self.meta['CRR_SCLY'])    
+
+    @classmethod
+    def is_datasource_for(cls, data, header, **kwargs):
+        """Determines if header corresponds to an 512 Channel image"""
+        return header.get('instrume') == 'SPECTROMAGNETOGRAPH'
