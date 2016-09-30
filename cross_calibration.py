Fixed rotation issue once and for all, moved functions into different files for plotting and quadrangle manipulation.

cross_calibration.py
@@ -3,68 +3,35 @@
 
 import zaw_util as z
 from zaw_coord import CRD
+import quadrangles as quad
+import block_plot as b
 import datetime as dt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import itertools
 from uncertainty import Measurement as M
-from astropy import units as u
-from astropy.io import fits
 import random
 import sunpy.time
 import getopt
 import sys
-import copy
 
 i1 = None       #Instrument 1
 i2 = None       #Instrument 2
 date = None     #Date to be examined
-
-class Block:
-
-    def __init__(self, mgnt, indices, ID, c=random.random()):
-        """Accepts heliographic bounds and a list of indices for block """
-        self.mgnt = mgnt
-        self.indices = indices
-        self.id = ID
-        self.pltColor = c
-        self.lat = (self.min_latitude(), self.max_latitude())
-        self.lon = (self.min_longitude(), self.max_longitude())
-
-    def min_latitude(self):
-        return M.nanmin(self.mgnt.lath[self.indices])
-
-    def max_latitude(self):
-        return M.nanmax(self.mgnt.lath[self.indices])
-
-    def min_longitude(self):
-        return M.nanmin(self.mgnt.lonh[self.indices])
-
-    def max_longitude(self):
-        return M.nanmax(self.mgnt.lonh[self.indices])
-
-    def mean_field(self):
-        return M.nanmean(self.mgnt.im_raw_u[self.indices])
-
-    def sum_flux(self):
-        return M.nansum(self.mgnt.mflux_corr[self.indices])
-
-    def sum_area(self):
-        return M.nansum(self.mgnt.area[self.indices])
-
+tol = 1      #Time difference tolerance
 
 def usage():
-    print('Usage: cross_calibration.py [-d data-root] [-f instrument 1] [-s instrument 2] [-i date]')
+    print('Usage: cross_calibration.py [-d data-root] [-f instrument 1] [-s instrument 2] [-i date] [-t tolerance]')
 
 def parse_args():
-    global i1, i2, date
+    global i1, i2, date, tol
 
     try:
         opts, args = getopt.getopt(
                 sys.argv[1:],
-                "d:f:s:i:", 
-                ["data-root=", "instrument-1=", "instrument-2=", "date="])
+                "d:f:s:i:t:", 
+                ["data-root=", "instrument-1=", "instrument-2=", "date=", "tol="])
     except getopt.GetoptError as err:
         print(err)
         usage()
@@ -79,58 +46,72 @@ def parse_args():
             i2 = arg
         elif opt in ("-i", "--date"):
             date = sunpy.time.parse_time(arg)
+        elif opt in ("-t", "--tol"):
+            tol = int(arg)
         else:
             assert False, "unhandled option"
 
-def find_match(date, f_list):
-    best = f_list[0]
-    bestTimeDelta = dt.timedelta(1)  #24 hour max
-
-    for f in f_list:
-        timeDelta = date - z.get_header_date(f)
-        if abs(timeDelta) < abs(bestTimeDelta):
-            bestTimeDelta = timeDelta
-            best = f
-
-    return best
-
-def process_date(i1, i2, date):
+def process_date(i1, i2, date, timeTolerance=1):
     fList1 = []
     fList2 = []
 
     #Include loop to include 24 hour difference
-    for i in range(-1, 2):
+    for i in range(0 - timeTolerance, 1 + timeTolerance):
         try:
-            fList1.extend(z.search_file(date + dt.timedelta(i), i1, auto=False))
-            fList2.extend(z.search_file(date, i2, auto=False))
+            f1 = z.search_file(date + dt.timedelta(i), i1, auto=False)
+            f2 = z.search_file(date, i2, auto=False)
+            if f1 == f2 and i1 != 'mdi' and i2 != 'mdi':
+                continue
+            else:
+                fList1.extend(f1)
+                fList2.extend(f2)
         except IOError:
             continue
+    result = list(set(itertools.product(fList1, fList2)))
 
+    if not result:
+        raise IOError
 
-    return list(set(itertools.product(fList1, fList2)))
+    return result
 
-def process_instruments(i1, i2):
+def process_instruments(i1, i2, retDates=False, timeTolerance=1):
     """Returns a list of valid file combinations.
 
     Searches for files within 48 hours of each other between instruments.
     """
-    start_i1, end_i1 = z.date_defaults(i1)
-    start_i2, end_i2 = z.date_defaults(i2)
+    if i1==i2:
+        if i1 in ('512', 'spmg'):
+            start_i1, end_i1 = z.date_defaults('512')
+            start_i2, end_i2 = z.date_defaults('spmg')
+        elif i1=='mdi':
+            start_i1, end_i1 = z.date_defaults('spmg')
+            start_i2, end_i2 = z.date_defaults('mdi')
+        else:
+            start_i1, end_i1 = z.date_defaults('mdi')
+            start_i2, end_i2 = z.date_defaults('hmi')
+    else:
+        start_i1, end_i1 = z.date_defaults(i1)
+        start_i2, end_i2 = z.date_defaults(i2)
 
     start = max(start_i1, start_i2)
     end = min(end_i1, end_i2)
     date = start
     files = []
+    dates = []
 
     while date < end:
         try:
-            files.extend(process_date(i1, i2, date))
+            files.extend(process_date(i1, i2, date, timeTolerance))
+            dates.append(date)
         except IOError:
             continue
         finally:
             date += dt.timedelta(1)
 
-    return list(set(files))
+    if retDates:
+        return list(set(files)), dates
+    else:
+        return list(set(files))
 
 def coordinate_compare(i1, i2):
     """A function used to compare two instruments longitude stats."""
@@ -179,258 +160,19 @@ def fix_longitude(f1, f2):
 
     m1 = CRD(f1)
     m2 = CRD(f2)
+    print(m1.im_raw.date)
+    print(m2.im_raw.date)
     m1.heliographic()
     m2.heliographic()
-    m1.magnetic_flux()
-    m2.magnetic_flux()
-    m1.lonh += 360
-    m2.lonh += 360
+    # Don't need right away for looking at consistency
+    # m1.magnetic_flux()
+    # m2.magnetic_flux()
     #Apply differential Rotation
     rotation = z.diff_rot(m1, m2)
-    m2.lonhRot = rotation.value + m2.lonh
-
-    #Compare reference pixels
-    # x1 = int(np.around(m1.X0))
-    # y1 = int(np.around(m1.Y0))
-    # x2 = int(np.around(m2.X0))
-    # y2 = int(np.around(m2.Y0))
-
-    # lonDelta = m2.lonhRot[x2, y2] - m1.lonh[x1, y1]
-    # m2.lonhOld = m2.lonh
-    # m2.lonh = m2.lonhRot - lonDelta
+    m2.lonhOld = m2.lonh
+    m2.lonh = rotation.value + m2.lonh
 
     return m1, m2
-
-def fragment_single(mgnt, n):
-    """
-    Takes in a magnetogram and n value and returns a list of block objects.
-
-    The magnetogram must have heliographic information stored (lonh and lath).
-    """
-    print("Processing {}".format(n))
-    fragInfo = get_fragmentation_info(mgnt, n)
-    return fragmentation_loop(fragInfo)
-
-def fragment_multiple(m1, m2, n):
-    """
-    Takes in two magnetograms and n value and returns a list of block objects.
-
-    The magnetograms must have heliographic information stored (lonh and lath).
-
-    """
-    if m1.X0 < m2.X0:
-        refmgnt = m1
-        secmgnt = m2
-    else:
-        refmgnt = m2
-        secmgnt = m1
-    print("Processing {}".format(n))
-    refFragInfo = get_fragmentation_info(refmgnt, n)
-    secFragInfo = get_fragmentation_info(secmgnt, n, refFragInfo['lonBands'])
-    refBlocks, secBlocks = fragmentation_loop(refFragInfo, secFragInfo)
-    
-    return refBlocks, secBlocks
-
-def get_fragmentation_info(m, n, lonBands=None):
-    m.lonh[m.rg > m.rsun*np.sin(85.0*np.pi/180)] = np.nan
-    m.lath[m.rg > m.rsun*np.sin(85.0*np.pi/180)] = np.nan
-    flatten(m)
-
-    # This supports reusing latitude and longitude bands for other mgnts.
-    latBands = split(n)
-    if lonBands is None:
-        lonBands = split(n, M.nanmin(m.lonh), M.nanmax(m.lonh))
-    lonMasks = []
-
-    for lon in lonBands:
-        lonMasks.append(m.lonh_1d > lon)
-
-    fragInfo = {'mgnt': m, 'latBands': latBands, 'lonBands': lonBands, 
-            'lonMasks': lonMasks}
-    return fragInfo
-
-def fragmentation_loop(refFragInfo, secFragInfo=None):
-    """
-    The main loop that finds valid indices for lat/lon conditions.
-
-    Input a dictionary of information for a single magnetogram, or
-    two if you want to compare two with the same heliographic bounds.
-    This dictionary must have the following valid keys:
-
-    mgnt: The magnetogram with flattened indices and heliographic information.
-    latBands: The interval space of latitudes to be used.
-    lonBands: The interval space of longitudes to be used.
-    lonMasks: The list of boolean masks for valid indices over lonBand space.
-    """
-    blocks = []
-    mgnt = refFragInfo['mgnt']
-    latBands = refFragInfo['latBands']
-    lonBands = refFragInfo['lonBands']
-    lonMasks = refFragInfo['lonMasks']
-    l = int(mgnt.im_raw.dimensions[0].value)
-    n = len(lonBands) - 1
-
-    if secFragInfo is not None:
-        secBlocks = []
-        secmgnt = secFragInfo['mgnt']
-        secLatBands = secFragInfo['latBands']
-        secLonBands = secFragInfo['lonBands']
-        secLonMasks = secFragInfo['lonMasks']
-        secCurrLat = (secmgnt.lath_1d > latBands[0])
-        secL = int(secmgnt.im_raw.dimensions[0].value)
-
-    currLat = (mgnt.lath_1d > latBands[0])
-    for i in range(n):
-        nextLat = (mgnt.lath_1d > latBands[i + 1])
-        latitudeSetDiff = currLat*~nextLat
-        if ~(latitudeSetDiff.any()):
-            continue
-        minLon = np.nanmin(mgnt.lonh_1d[latitudeSetDiff])
-        maxLon = np.nanmax(mgnt.lonh_1d[latitudeSetDiff])
-
-        latBounds = (latBands[i], latBands[i + 1])
-        areaRatio = calc_area_ratio(latBands, lonBands, latBounds)
-        s = max(np.searchsorted(lonBands, minLon) - 1, 0)
-        e = np.searchsorted(lonBands, maxLon)
-        skip = int(round(areaRatio))
-
-        if secFragInfo is not None:
-            secNextLat = (secmgnt.lath_1d > latBands[i + 1])
-            secLatitudeSetDiff = secCurrLat*~secNextLat
-
-        for j in range(s, e, skip):
-            uuid = str(i) + str(j) + str(n)
-            try:
-                blockBool = lonMasks[j]*~lonMasks[j+skip]*latitudeSetDiff
-            except IndexError:
-                blockBool = lonMasks[j]*~lonMasks[-1]*latitudeSetDiff
-            if ~(blockBool.any()):
-                continue
-            else:
-                c = random.random()
-                blockInd = transform_indices(mgnt.ind_1d[blockBool], l)
-                x = Block(mgnt, blockInd, uuid, c)
-                blocks.append(x)
-            if secFragInfo is not None:
-                try:
-                    secBlockBool = secLonMasks[j]*~secLonMasks[j+skip]*secLatitudeSetDiff
-                except IndexError:
-                    secBlockBool = secLonMasks[j]*~secLonMasks[-1]*secLatitudeSetDiff
-                if ~(secBlockBool.any()):
-                    continue
-                secBlockInd = transform_indices(secmgnt.ind_1d[secBlockBool], secL)
-                y = Block(secmgnt, secBlockInd, uuid, c)
-                secBlocks.append(y)
-        currLat = nextLat
-        if secFragInfo is not None:
-            secCurrLat = secNextLat
-
-    if secFragInfo is not None:
-        return blocks, secBlocks
-    else:
-        return blocks
-
-def split(n, minimum=-90, maximum=90):
-    """Returns an interval space based on n number of blocks."""
-    return np.linspace(minimum, maximum, n + 1)
-
-def flatten(m):
-    """
-    Takes in a magnetogram and adds 1 dimensional flattened
-    attributes for heliographic information.
-    """
-
-    m.lonh_1d = m.lonh.v.flatten()
-    m.lath_1d = m.lath.v.flatten()
-    m.ind_1d = np.arange(np.size(m.lath_1d))
-
-    ind = np.where(M.isfinite(m.lonh_1d))
-
-    m.lonh_1d = m.lonh_1d[ind]
-    m.ind_1d = m.ind_1d[ind]
-    m.lath_1d = m.lath_1d[ind]
-
-def calc_area_ratio(latBands, lonBands, latBounds):
-    """
-    Calculates the area ratio of the given latitude band.
-
-    The middle square solid angle is the centermost block of the sun.
-    This is used for reference because arguably it is the biggest block given
-    certain lat/lon bounds. This is used in skipping n values for a more
-    comprehensive approach to enlarging the blocks near the poles for area
-    similarities.
-    """
-    n = len(latBands) - 1
-    lonBlockDiff = lonBands[n//2 + 1] - lonBands[n//2] 
-
-    middleSquareSA = (np.sin(latBands[n//2 + 1]*np.pi/180) - np.sin(latBands[n//2]*np.pi/180))*lonBlockDiff
-    sa = (np.sin(latBounds[1]*np.pi/180) - np.sin(latBounds[0]*np.pi/180))*lonBlockDiff
-    areaRatio = middleSquareSA/sa
-
-    return areaRatio
-
-def transform_indices(ind, l):
-    """Takes a 1D list of indices and calculates a 2D list of indices."""
-
-    cols = ind % l
-    rows = ind // l
-    return (rows, cols)
-
-def block_area(mgnt, blocks):
-    """Given a list of blocks, will calculate and print out areas."""
-
-    areas = []
-    for block in blocks:
-        try:
-            areas.append(block.sum_area())
-        except:
-            areas.append(M(np.nan, np.nan))
-
-    return areas
-
-def block_field(mgnt, blocks, raw=False):
-    """Given a list of blocks, will calculate and print out areas."""
-
-    field = []
-    for block in blocks:
-        try:
-            field.append(block.mean_field())
-        except:
-            field.append(M(np.nan, np.nan))
-    return field
-
-def block_flux(mgnt, blocks):
-    """Given a list of blocks, will calculate and print out areas."""
-
-    flux = []
-    for block in blocks:
-        try:
-            flux.append(block.sum_flux())
-        except:
-            flux.append(M(np.nan, np.nan))
-
-    return flux
-
-def block_plot(*args):
-    """Given a list of blocks, will plot a nice image differentiating them."""
-    n = len(args)
-    rows = int(round(np.sqrt(n)))
-    cols = int(np.ceil(np.sqrt(n)))
-    im = {}
-    ax = {}
-    for i in range(len(args)):
-        if isinstance(args[i], type(list())):
-            im[i] = args[i][0].mgnt.lonh.v.copy()
-            for x in args[i]:
-                im[i][x.indices] = x.pltColor
-            ax[i] = plt.subplot2grid((rows, cols), (i%rows, i//rows))
-            ax[i].imshow(im[i], vmin=0, vmax=1)
-            ax[i].set_title(args[i][0].mgnt.im_raw.date)
-        else:
-            im[i] = args[i]
-            ax[i] = plt.subplot2grid((rows, cols), (i%rows, i//rows))
-            ax[i].imshow(im[i], cmap='binary')
-    plt.show()
 
 def run_multiple_n(m):
     nList = [i for i in range(10, 3100, 100)]
@@ -438,82 +180,76 @@ def run_multiple_n(m):
     n_dict_length = {}
 
     for n in nList:
-        N = fragment(m, n)
+        N = quad.fragment_single(m, n)
         n_dict_length[n] = len(N)
-    return 
+    return n_dict_length
 
-def scatter_plot(dict1, dict2, separate=False):
-    i = 1
-    for n in sorted([x for x in dict1.keys() if x != 'instr']):
-        if separate:
-            plt.subplot(len(dict1), 1, i)
-            plt.xlabel(dict1['instr'])
-            plt.ylabel(dict2['instr'])
-            plt.title('Number of blocks: {}'.format(n))
-        plt.plot([np.nanmin(dict1[n]),np.nanmax(dict1[n])],[np.nanmin(dict1[n]),np.nanmax(dict1[n])], 'k-', lw=2)
-        plt.plot(dict1[n], dict2[n], '.')
-        i += 1
+def compare_day(i1, i2, n, date=None, f1=None, f2=None):
+    """
+    Fully autonomous magnetogram comparison function.
+
+    Can input two instruments for a random sampling of their matches.
+    If date is entered, it will compare magnetograms for that date.
+    f1 and f2 are filenames that bypass all of this
+    """
+    if f1 is not None or f2 is not None:
+        files = (f1, f2)
+    elif date is None:
+        files = random.choice(process_instruments(i1, i2, timeTolerance=tol))
+
+    m1, m2 = fix_longitude(files[0], files[1])
+    i1_n, i2_n = quad.fragment_multiple(m1, m2, n)
+    p_i1 = quad.calc_block_parameters(m1, i1_n)
+    p_i2 = quad.calc_block_parameters(m2, i2_n)
+
+    ax2 = b.block_plot(i1_n, i2_n)
+    ax1 = b.plot_block_parameters(p_i1, p_i2)
     plt.show()
 
-def calc_n_lists(m, block):
-    ar = {}
-    flx = {}
-    f = {}
-    for key, value in block.items():
-        ar[key], flx[key], f[key] = calc_list_parameters(m, value)
-    ar['instr'] = m.im_raw.instrument
-    flx['instr'] = m.im_raw.instrument
-    f['instr'] = m.im_raw.instrument
-    return ar, flx, f
+def get_instruments():
+    global i1, i2
+    i1 = input("Enter an instrument: ")
+    i2 = input("Enter a second instrument: ")
 
-def calc_list_parameters(m, blockList):
-    ar = block_area(m, blockList)
-    flx = block_flux(m, blockList)
-    f = block_field(m, blockList)
-    return ar, flx, f
-
-def n_plot(n_dict):
-    NList = []
-    TList = []
-    for key, value in n_dict.items():
-        NList.append(key)
-        TList.append(len(value))
-    N = np.array(NList)
-    T = np.array(TList)
-    plt.plot(N, T, '.')
-
-    plt.show()
-
-def plot_instrument_comparisons():
-    pass
-
-def compare_day(i1, i2, n, date=None):
-    if date is None:
-        files = process_instruments(i1, i2)
-    else:
-        date = sunpy.time.parse_time(date)
-        files = process_date(i1, i2, date)
-        prompt = "Enter another date, CTRL-C to cancel: "
-        while files == []:
-            try:
-                date = sunpy.time.parse_time(input(prompt))
-            except KeyboardInterrupt:
-                break
-            files = process_date(i1, i2, date)
-    m1, m2 = fix_longitude(files[0][0], files[0][1])
-    i1_n, bands = fragment(m1, n)
-    i2_n = fragment(m2, n, bands[0], bands[1])[0]
-    ar_i1, flx_i1, f_i1 = calc_list_parameters(m1, i1_n)
-    ar_i2, flx_i2, f_i2 = calc_list_parameters(m2, i2_n)
-
+def select_pair():
+    files, dates = process_instruments(i1, i2, True, tol)
+    print(set([x.year for x in dates]))
+    y = input("Choose a year: ")
+    print(set([x.month for x in dates if x.year == int(y)]))
+    m = input("Choose a month: ")
+    print(set([x.day for x in dates if x.year == int(y) and x.month == int(m)]))
+    d = input("Choose a day: ")
+    t = [x for x in dates if x.year == int(y) and x.month == int(m) and x.day == int(d)][0]
+    files = process_date(i1, i2, t, tol)
+    k = 0
+    for line in files:
+        print("{}: ({}, {})".format(k, line[0].split('\\')[-1], line[1].split('\\')[-1]))
+        k += 1
+    choice = int(input("Select a pair: "))
+    return files[choice]
 
 def main():
-    global i1, i2
     parse_args()
-    files = process_instruments('spmg', 'mdi')
-    m1, m2 = fix_longitude(files[0][0], files[0][1])
-    fragment_multiple(m1, m2, 9)
-    
+    if i1 is None or i2 is None:
+        get_instruments()
+
+    while True:
+        try:
+            option = input("Choose a function: (r)andom, (s)elect date, switch (i)nstruments: ")
+            if option=='i':
+                get_instruments()
+            elif option=='r':
+                n = int(input("Enter segmentation level: "))
+                compare_day(i1, i2, n)
+            else:
+                file1, file2 = select_pair()
+                n = int(input("Enter segmentation level: "))
+                compare_day(i1, i2, n, f1=file1, f2=file2)
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(e)
+            continue
 
 if __name__ == "__main__":
     main()

zaw_coord.py
@@ -35,6 +35,7 @@ class CRD:
             self.Y0 = self.im_raw.meta['CRPIX2A']
             self.B0 = M(self.im_raw.meta['B0'], np.abs(self.im_raw.meta['B0'])*.01)
             self.L0 = M(self.im_raw.meta['L0'], np.abs(self.im_raw.meta['L0'])*.01)
+            self.SL0 = M(0, 0)
             self.xScale = M(self.im_raw.scale[0].value, 0.002)
             self.yScale = M(self.im_raw.scale[1].value, 0.002)
             self.rsun = M(self.im_raw.rsun_obs.value, 1)
@@ -47,46 +48,47 @@ class CRD:
             self.Y0 = self.im_raw.meta['CRPIX2A']
             self.B0 = M(self.im_raw.meta['B0'], np.abs(self.im_raw.meta['B0'])*.01)
             self.L0 = M(self.im_raw.meta['L0'], np.abs(self.im_raw.meta['L0'])*.01)
+            self.SL0 = M(0, 0)
             self.xScale = M(self.im_raw.scale[0].value, 0)
             self.yScale = M(self.im_raw.scale[1].value, 0)
             self.rsun = M(self.im_raw.rsun_obs.value, 1)
             self.dsun = self.DSUN_METERS
 
         elif self.im_raw.detector == 'MDI':
-
-            # Define center of sun and location of detector.
-            self.X0 = self.im_raw.meta['X0']
-            self.Y0 = self.im_raw.meta['Y0']
-            try:
-                self.B0 = M(self.im_raw.meta['B0'], np.abs(self.im_raw.meta['B0'])*.01)
-                self.L0 = M(self.im_raw.meta['L0'], np.abs(self.im_raw.meta['L0'])*.01)
-            except KeyError:
-                self.B0 = M(self.im_raw.meta['OBS_B0'], np.abs(self.im_raw.meta['OBS_B0'])*.01)
-                self.L0 = M(self.im_raw.meta['OBS_L0'], np.abs(self.im_raw.meta['OBS_L0'])*.01)
-            self.xScale = M(1.982, 0.003)
-            self.yScale = M(1.982, 0.003)
             self.rsun = M(self.im_raw.rsun_obs.value, 1)
             self.dsun = M(self.im_raw.dsun.value, 0)
             try:
                 self.P0 = self.im_raw.meta['p_angle']
             except KeyError:
                 self.P0 = self.im_raw.meta['solar_p']
-
             if self.P0 != 0:
-                self.im_raw = self.im_raw.rotate(angle=self.P0*u.deg)
+                self.im_raw = self.im_raw.rotate(angle=-self.P0*u.deg)
+            # Define center of sun and location of detector.
+            self.X0, self.Y0 = (x.value for x in self.im_raw.reference_pixel)
+            try:
+                self.B0 = M(self.im_raw.meta['B0'], np.abs(self.im_raw.meta['B0'])*.01)
+                self.L0 = M(self.im_raw.meta['L0'], np.abs(self.im_raw.meta['L0'])*.01)
+            except KeyError:
+                self.B0 = M(self.im_raw.meta['OBS_B0'], np.abs(self.im_raw.meta['OBS_B0'])*.01)
+                self.L0 = M(self.im_raw.meta['OBS_L0'], np.abs(self.im_raw.meta['OBS_L0'])*.01)
+            self.SL0 = self.L0 - sun.heliographic_solar_center(self.im_raw.date)[0].value
+            self.xScale = M(1.982, 0.003)
+            self.yScale = M(1.982, 0.003)
+            
 
         elif self.im_raw.detector == 'HMI':
-            self.X0 = self.im_raw.meta['CRPIX1']
-            self.Y0 = self.im_raw.meta['CRPIX2']
-            self.B0 = M(self.im_raw.meta['CRLT_OBS'], np.abs(self.im_raw.meta['CRLT_OBS'])*.01)
-            self.L0 = M(self.im_raw.meta['CRLN_OBS'], np.abs(self.im_raw.meta['CRLN_OBS'])*.01)
-            self.xScale = M(self.im_raw.scale[0].value, 0.001)
-            self.yScale = M(self.im_raw.scale[1].value, 0.001)
             self.rsun = M(self.im_raw.rsun_obs.value, 1)
             self.dsun = M(self.im_raw.dsun.value, 0)
             self.P0 = self.im_raw.meta['CROTA2']
             if self.P0 != 0:
                 self.im_raw.rotate(self.P0*u.deg)
+            self.X0, self.Y0 = (x.value for x in self.im_raw.reference_pixel)
+            self.B0 = M(self.im_raw.meta['CRLT_OBS'], np.abs(self.im_raw.meta['CRLT_OBS'])*.01)
+            self.L0 = M(self.im_raw.meta['CRLN_OBS'], np.abs(self.im_raw.meta['CRLN_OBS'])*.01)
+            self.SL0 = self.L0 - sun.heliographic_solar_center(self.im_raw.date)[0].value
+            self.xScale = M(self.im_raw.scale[0].value, 0.001)
+            self.yScale = M(self.im_raw.scale[1].value, 0.001)
+            
 
         else:
             print ("Not a valid instrument or missing header information regarding instrument.")
@@ -139,8 +141,9 @@ class CRD:
         rx, ry, rz = self._hpc_hcc(x, y)
 
         # Now convert to heliographic coordinates.
-        lonh, lath = self._hcc_hg(rx, ry, rz)
+        lonh, lath = self._hcc_hg(rx, ry, rz, self.B0, self.SL0)
 
+        self.im_raw.data[self.rg > self.rsun] = np.nan
         # Only add the instance attribute if it doesn't exist.
         if array and not hasattr(self, 'lonh') and not corners:
             self.lonh = lonh
@@ -169,10 +172,10 @@ class CRD:
             lonh, lath = M.deg2rad(self.heliographic(args[0], args[1]))
 
         B0 = M.deg2rad(self.B0)
-        L0 = M.deg2rad(self.L0)
+        SL0 = M.deg2rad(self.SL0)
 
-        Xobs = M.cos(B0)*M.cos(L0)
-        Yobs = M.cos(B0)*M.sin(L0)
+        Xobs = M.cos(B0)*M.cos(SL0)
+        Yobs = M.cos(B0)*M.sin(SL0)
         Zobs = M.sin(B0)
 
         corr_factor = (M.cos(lath)*M.cos(lonh)*Xobs
@@ -339,7 +342,7 @@ class CRD:
 
         return rx, ry, rz
 
-    def _hcc_hg(self, x, y, z):
+    def _hcc_hg(self, x, y, z, B0=0, L0=0):
         """Converts hcc coordinates to Stonyhurst heliographic. 
 
         x - x coordinate in meters
@@ -348,12 +351,12 @@ class CRD:
         Calculations taken and shortened
         from sunpy.wcs.
         """
-        cosb = M.cos(M.deg2rad(self.B0))
-        sinb = M.sin(M.deg2rad(self.B0))
+        cosb = M.cos(M.deg2rad(B0))
+        sinb = M.sin(M.deg2rad(B0))
 
         hecr = M.sqrt(x**2 + y**2 + z**2)
         hgln = M.arctan2(x, z*cosb - y*sinb) \
-                + M.deg2rad(self.L0)
+                + M.deg2rad(L0)
         hglt = M.arcsin((y * cosb + z * sinb)/hecr)
 
         return hgln*180/np.pi, hglt*180/np.pi