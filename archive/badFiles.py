e5c0ba330d84dfe94c48b1bab503437ff9425b7c
Zach Werginz <zachary.werginz@snc.edu>
2017-07-25 09:58:43 -0500

    archived/deprecated files

---
+++ archive/badFiles.py
@@ -0,0 +1,29 @@
+import cross_calibration as c
+from zaw_coord import CRD
+import numpy as np
+import psycopg2 as psy
+import zaw_util as z
+
+def main():
+    conn = z.load_database()
+    cur = conn.cursor()
+    cur.execute("SELECT filepath FROM file WHERE instrument=3")
+    files = cur.fetchall()
+    cur.close()
+    badFiles = []
+
+    for f in files:
+        try:
+            m = CRD(f[0])
+            latitudeMask = np.where(m.lath.v < 50)
+        except AttributeError:
+            continue
+        except KeyError:
+            continue
+        except ValueError as e:
+            print(e)
+            continue
+
+        if np.nanmax(np.abs(m.lonh.v[latitudeMask])) > 90:
+            badFiles.append(f[0])
+    return badFiles
---
+++ archive/n_distributions.py
@@ -0,0 +1,56 @@
+import zaw_util as z
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+from itertools import cycle
+
+def main():
+    instrumentKey = {'512': 1, 'spmg': 2, 'mdi': 3, 'hmi': 4}
+    colors = [(255/255, 208/255, 171/255), 
+            (114/255, 178/255, 229/255), 
+            (81/255, 178/255, 76/255),
+            (111/255, 40/255, 124/255),
+            (80/255, 60/255, 0/255)]
+    colorcycler = cycle(colors)
+    i = '512'
+
+    print("Downloading data for {}...".format(i))
+    dataDict = {}
+
+    if i == '512':
+        for n in [25, 50, 100, 200, 400]:
+            dataDict[str(n)] = z.download_cc_data('512', '512', n, '-49 hours', '49 hours')['referenceFD']
+            dataDict[str(n)] = np.append(dataDict[str(n)], z.download_cc_data('512', 'spmg', n, '-49 hours', '49 hours')['referenceFD'])
+    elif i == 'spmg':
+        for n in [25, 50, 100, 200, 400]:
+            dataDict[str(n)] = z.download_cc_data('spmg', 'spmg', n, '-49 hours', '49 hours')['referenceFD']
+            dataDict[str(n)] = np.append(dataDict[str(n)], z.download_cc_data('spmg', 'mdi', n, '-49 hours', '49 hours')['referenceFD'])
+    elif i == 'mdi':
+        for n in [25, 50, 100, 200, 400]:
+            dataDict[str(n)] = z.download_cc_data('mdi', 'mdi', n, '-49 hours', '49 hours')['referenceFD']
+    elif i == 'hmi':
+        for n in [25, 50, 100, 200, 400]:
+            dataDict[str(n)] = z.download_cc_data('mdi', 'hmi', n, '-49 hours', '49 hours')['referenceFD']
+
+    for k, value in sorted(dataDict.items(), key=lambda x: int(x[0]), reverse=True):
+        c = next(colorcycler)
+        n1, bins1, patches1 = plt.hist(value, 50, normed=True, histtype='stepfilled', color=c, alpha=1, edgecolor='none', label=k)
+
+    for k, value in sorted(dataDict.items(), key=lambda x: int(x[0]), reverse=True):
+        c = next(colorcycler)
+        n2, bins2, patches2 = plt.hist(value, bins=bins1, normed=True, histtype='step', color=c, alpha=1)  
+
+    ax = plt.gca()
+    ax.set_ylabel('Frequency')
+    ax.set_xlabel('Magnetic Flux Density (G)')
+    ax.set_yscale('log')
+    ax.set_xlim(-4600, 4600)
+    f = plt.gcf()
+    fig_title = i
+    f.suptitle(fig_title, y=.95, fontsize=30, fontweight='bold')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
